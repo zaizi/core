ce728c97903a7086b73168b96ae8cba74d386046
Carlos Ortiz <carlos.ortiz@craftercms.org>
2015-02-11 17:34:12 -0600

    prepare for next development iteration

---
+++ src/mainjava/org/craftercms/core/cache/impl/CacheItemImpl.java
@@ -0,0 +1,213 @@
+/*
+ * Copyright (C) 2007-2013 Crafter Software Corporation.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.craftercms.core.cache.impl;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.craftercms.core.cache.CacheItem;
+import org.craftercms.core.cache.CacheLoader;
+
+/**
+ * Default implementation of {@link org.craftercms.core.cache.CacheItem}.
+ *
+ * @author Sumer Jabri
+ * @author Alfonso Vásquez
+ */
+public class CacheItemImpl implements CacheItem {
+
+    protected final String scope;
+    protected final long ticksAtCreation;
+
+    protected final Object key;
+    protected final Object value;
+
+    protected final long ticksToExpire;
+    protected final long ticksToRefresh;
+
+    protected final long timestamp;
+    protected final List<Object> dependencyKeys;
+
+    protected final CacheLoader loader;
+    protected final Object[] loaderParams;
+
+    /**
+     * Value constructor.
+     */
+    public CacheItemImpl(final String scope, final long ticksAtCreation, final Object key, final Object value,
+                         final long ticksToExpire, final long ticksToRefresh, final long timestamp,
+                         final List<Object> dependencyKeys, final CacheLoader loader, final Object[] loaderParams) {
+        this.scope = scope;
+        this.ticksAtCreation = ticksAtCreation;
+
+        this.key = key;
+        this.value = value;
+
+        this.ticksToExpire = ticksToExpire;
+        this.ticksToRefresh = ticksToRefresh;
+
+        this.timestamp = timestamp;
+        this.dependencyKeys = dependencyKeys;
+
+        this.loader = loader;
+        this.loaderParams = loaderParams;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getScope() {
+        return this.scope;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Object getKey() {
+        return this.key;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Object getValue() {
+        return this.value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public long getTicksAtCreation() {
+        return this.ticksAtCreation;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public long getTicksToExpire() {
+        return this.ticksToExpire;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public long getTicksToRefresh() {
+        return this.ticksToRefresh;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public CacheLoader getLoader() {
+        return this.loader;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Object[] getLoaderParams() {
+        return this.loaderParams;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public long getTimestamp() {
+        return this.timestamp;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Object> getDependencyKeys() {
+        return this.dependencyKeys;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isExpired(long currentTicks) {
+        return this.ticksToExpire != NEVER_EXPIRE && currentTicks >= (this.ticksAtCreation + this.ticksToExpire);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean needsRefresh(long currentTicks) {
+        return this.ticksToRefresh != NEVER_REFRESH && currentTicks >= (this.ticksAtCreation + this.ticksToRefresh);
+    }
+
+    /**
+     * Returns true if the specified {@code CacheItemImpl}'s and this instance's key and scope are equal.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        CacheItemImpl item = (CacheItemImpl)o;
+
+        if (!this.key.equals(item.key)) {
+            return false;
+        }
+        if (!this.scope.equals(item.scope)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = this.scope.hashCode();
+        result = 31 * result + this.key.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "CacheItemImpl[" +
+            "scope='" + this.scope + '\'' +
+            ", ticksAtCreation=" + this.ticksAtCreation +
+            ", key=" + this.key +
+            ", value=" + this.value +
+            ", ticksToExpire=" + this.ticksToExpire +
+            ", ticksToRefresh=" + this.ticksToRefresh +
+            ", timestamp=" + this.timestamp +
+            ", dependencyKeys=" + this.dependencyKeys +
+            ", loader=" + this.loader +
+            ", loaderParams=" + (this.loaderParams == null? null: Arrays.asList(this.loaderParams)) +
+            ']';
+    }
+
+}
---
+++ src/mainjava/org/craftercms/core/url/impl/AddSchemeAndDomainAndServerPortUrlTransformer.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2007-2013 Crafter Software Corporation.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.craftercms.core.url.impl;
+
+import org.craftercms.core.exception.UrlTransformationException;
+import org.craftercms.core.service.CachingOptions;
+import org.craftercms.core.service.Context;
+import org.craftercms.core.url.UrlTransformer;
+import org.craftercms.core.util.HttpServletUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Prepends to a context relative url the schema, domain and port (if different than 80 and 443 in case of https) to
+ * URL, normally to get a
+ * full url, e.g., /webapp/servlet/a => http://domain.com:8080/webapp/servlet/a
+ *
+ * @author Alfonso Vásquez
+ */
+public class AddSchemeAndDomainAndServerPortUrlTransformer implements UrlTransformer {
+
+    public static final String HTTP_SCHEME =        "http";
+    public static final String HTTPS_SCHEME =       "https";
+    public static final int DEFAULT_HTTP_PORT =     80;
+    public static final int DEFAULT_HTTPS_PORT =    443;
+
+    protected boolean forceHttps;
+    protected int httpsPort;
+
+    public AddSchemeAndDomainAndServerPortUrlTransformer() {
+        forceHttps = false;
+        httpsPort = DEFAULT_HTTPS_PORT;
+    }
+
+    public void setForceHttps(boolean forceHttps) {
+        this.forceHttps = forceHttps;
+    }
+
+    public void setHttpsPort(int httpsPort) {
+        this.httpsPort = httpsPort;
+    }
+
+    @Override
+    public String transformUrl(Context context, CachingOptions cachingOptions,
+                               String url) throws UrlTransformationException {
+        HttpServletRequest currentRequest = HttpServletUtils.getCurrentRequest();
+        String scheme = currentRequest.getScheme();
+        String domain = currentRequest.getServerName();
+        int serverPort = currentRequest.getServerPort();
+
+        if (forceHttps) {
+            scheme = HTTPS_SCHEME;
+            serverPort = httpsPort;
+        }
+
+        StringBuilder fullUrl = new StringBuilder();
+        fullUrl.append(scheme).append("://").append(domain);
+
+        if (!(scheme.equals(HTTP_SCHEME) && serverPort == DEFAULT_HTTP_PORT) &&
+            !(scheme.equals(HTTPS_SCHEME) && serverPort == DEFAULT_HTTPS_PORT)) {
+            fullUrl.append(":").append(serverPort);
+        }
+
+        if (!url.startsWith("/")) {
+            fullUrl.append("/");
+        }
+
+        fullUrl.append(url);
+
+        return fullUrl.toString();
+    }
+
+}
